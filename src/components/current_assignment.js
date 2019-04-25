1eebf351ca7e060b0ed8ee4267c892fc6ec6aaa8
baileyburke <49003818+baileyburke@users.noreply.github.com>
2019-04-25 15:36:00 -0400

    Add files via upload

---
+++ src/components/current_assignment.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import classnames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+// More imports here
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import IconButton from '@material-ui/core/IconButton';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import CardMedia from '@material-ui/core/CardMedia';
+import ProgressBar from './progress_bar';
+import AssignmentStepper from './assignment_stepper';
+
+
+class CurrentAssignment extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeStep: 1,
+      expanded: false,
+    };
+  }
+
+  // handlers will go here
+  handleStepChange = (newStep) => {
+    this.setState({
+      activeStep: newStep,
+    });
+  };
+
+  handleExpandClick = () => {
+    this.setState(prevState => ({
+      expanded: !prevState.expanded,
+    }));
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { assignment } = this.props;
+
+
+    return (
+
+      <Card className={classes.root}>
+        <CardHeader
+          title="Current Assignment"
+          subheader={assignment.title}
+        />
+        <CardMedia
+          className={classes.media}
+          image={assignment.image}
+          title={assignment.title}
+        />
+
+        <CardContent>
+          <ProgressBar activeStep={this.state.activeStep} steps={assignment.steps} />
+          <div className={classes.descriptionContainer}>
+            <p className={classes.description}>{assignment.description.long}</p>
+            <IconButton
+              className={classnames(classes.expand, {
+                [classes.expandOpen]: this.state.expanded,
+              })}
+              onClick={this.handleExpandClick}
+              aria-expanded={this.state.expanded}
+              aria-label="Show more"
+            >
+              <ExpandMoreIcon />
+            </IconButton>
+          </div>
+
+        </CardContent>
+        {this.state.expanded ? <AssignmentStepper activeStep={this.state.activeStep} steps={assignment.steps} handleStepChange={this.handleStepChange} /> : null}
+
+      </Card>
+    );
+  }
+}
+
+const styles = {
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    textAlign: 'center',
+    marginTop: '20px',
+    marginBottom: '20px',
+    width: '60%',
+  },
+  media: {
+    height: 0,
+    paddingTop: '40%',
+    backgroundSize: 'cover',
+  },
+  expand: {
+    marginLeft: 5,
+  },
+  expandOpen: {
+    marginLeft: 5,
+    transform: 'rotate(180deg)',
+  },
+  descriptionContainer: {
+    marignTop: 15,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  description: {
+    maxWidth: '80%',
+  },
+};
+
+// needed to override styles
+export default withStyles(styles)(CurrentAssignment);
---
+++ src/components/progress_bar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+const ProgressBar = (props) => {
+  // Declare important variables here
+  const MIN = 0;
+  const MAX = props.steps.length;
+  const normalise = activeStep => (activeStep - MIN) * 100 / (MAX - MIN);
+
+
+  return (
+    // Create progress bar here
+    <div>
+      <React.Fragment>
+        <LinearProgress className={props.classes.progressBar} variant="determinate" value={normalise(props.activeStep)} />
+      </React.Fragment>
+    </div>
+  );
+};
+
+const styles = {
+  progressBar: {
+    background: '#babed6',
+    borderRadius: 3,
+    height: 10,
+    boxShadow: '0 3px 5px 2px rgba(25, 33, 109, .3)',
+  },
+};
+
+export default withStyles(styles)(ProgressBar);
